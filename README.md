2d00ce585d67109a57e49528cc1c8036d2f7f121
Luis Roberto <luis.roberto@luka.la>
2021-04-04 16:30:06 -0400

    Configuracion adicional para autoformato con prettier

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 1. Crear un proyecto node
-   npm init -y
+
+   `npm init -y`
 
 2. Instalar Cucumber
-   npm i -D @cucumber/cucumber
+
+   `npm i -D @cucumber/cucumber`
 
 3. Instalar chai
-   npm i -D chai
+
+   `npm i -D chai`
 
 4. Opcional: Crear repositorio
-   git init
+
+   `git init`
--- features/steps/suma.steps.js
+++ features/steps/suma.steps.js
@@ -1,14 +1,14 @@
-const { Given, When, Then } = require("@cucumber/cucumber");
-const { expect } = require("chai");
+const { Given, When, Then } = require('@cucumber/cucumber')
+const { expect } = require('chai')
 
-Given("Una calculadora", function () {
+Given('Una calculadora', function () {
   // ya en world se creo la calculadora
-});
+})
 
-When("Se ingresan {int} y {int}", function (int, int2) {
-  this.result = this.calculator.sum(int, int2);
-});
+When('Se ingresan {int} y {int}', function (int, int2) {
+  this.result = this.calculator.sum(int, int2)
+})
 
-Then("El resultado debe ser {int}", function (int) {
-  expect(this.result).to.be.equal(int);
-});
+Then('El resultado debe ser {int}', function (int) {
+  expect(this.result).to.be.equal(int)
+})
